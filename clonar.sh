b5abbfd9254f7d2ffba0bb7b5fe32baad6d36651
Joshua <ccquicenol@uqvirtual.edu.co>
2020-11-29 01:16:26 -0500

    Comando bash mejorado

--- clonar.sh
+++ clonar.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-DIRECTORIO=Github/Proyecto_BI
+DIRECTORIO= Proyecto_BI
 
 if [ ! -d "$DIRECTORIO" ]
 then
 
-cd Github/ ; git clone https://github.com/camilosky6/Proyecto_BI.git ; chmod 755 Proyecto_BI ; chmod 755 Proyecto_BI/init.py ; pip3 install -r Proyecto_BI/requirements.txt 
+git clone https://github.com/camilosky6/Proyecto_BI.git 
+chmod 755 -R Proyecto_BI 
+pip3 install -r Proyecto_BI/requirements.txt 
 
 echo "el proyecto  NO existe $DIRECTORIO se clono exitosamente"
-else 
+else
 
 echo "el directorio  existe $DIRECTORIO, no se puede clonar "
 
